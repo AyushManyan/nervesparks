95bd2591bedcd3c8e11cd54b683f455cab6d688d
Ayush <ayush.manyan@lpu.in>
2025-08-15 17:03:23 +0530

    update the cores

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,8 +13,10 @@ import healthRoutes from './routes/healthRoutes.js';
 import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
+const cors = require('cors');
 app.use(cors({
-    origin:"*"
+  origin: 'http://localhost:5173',
+  credentials: true
 }));
 
 app.use(express.json({ limit: '2mb' }));
